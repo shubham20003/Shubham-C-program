3bcf1528a3f8afa7d6c46ec181be1da6513bb13c
shubham mhase <104731758+shubham20003@users.noreply.github.com>
2024-08-08 09:14:50 +0530

    Add files via upload

---
+++ float.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    printf("enter a");
+    scanf("%f",&a);
+
+    printf("enter b");
+    scanf("%f",&b);
+
+    printf("product of two numbers is %f \n",a*b);
+    printf("sum of two numbers is %f \n",a+b);
+    printf("division of two numbers is %f \n",a/b);
+
+
+}
