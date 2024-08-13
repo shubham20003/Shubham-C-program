e62dab2472c0cb83d4cc65deec1543c734a8f990
shubham mhase <104731758+shubham20003@users.noreply.github.com>
2024-08-13 22:48:18 +0530

    Add files via upload

---
+++ day_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    char day;
+    printf("enter the day");
+    scanf("%s",&day);
+
+    switch(day){
+            case 'm' : printf("monday");
+            break;
+
+            case 't' : printf("tuesday");
+            break;
+
+            case 'w' : printf("wednesday");
+            break;
+
+            
+
+
+
+    }
+    return 0;
+}
