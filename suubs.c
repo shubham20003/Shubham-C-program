b073d9ca88c3fc1b155b3b6963f254f170e4b157
SHUBHAM MHASE <104731758+shubham20003@users.noreply.github.com>
2024-08-16 23:42:58 +0530

    Add files via upload

---
+++ suubs.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+
+    printf("enter b");
+    scanf("%d",&b);
+
+    printf("the division of a and b is %d\n",a/b);
+
+    printf("the multiplication of a and b is %d\n",a*b);
+
+    printf("the addition of a and b is %d\n",a+b);
+
+    printf("the substraction of a and b %d\n",a-b);
+}
