7779849318467904db3a62b202a1c8888b50606f
SHUBHAM MHASE <104731758+shubham20003@users.noreply.github.com>
2024-08-17 12:50:25 +0530

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    while(i<=20){
+        if(i>=10){
+            printf("natural numbers between 10 to 20 %d\n",i);
+            i++;
+        }
+    }
+}
