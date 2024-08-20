5213b9a9a6f9bf8705bcf976266070d08bbe166f
SHUBHAM MHASE <104731758+shubham20003@users.noreply.github.com>
2024-08-20 11:00:34 +0530

    Add files via upload

---
+++ assign_3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i=0;
+
+    do{
+        printf("Welcome");
+        i++;
+    }
+    while(i<1);
+}
---
+++ assign_4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int sum=0;
+    int i;
+
+    for(i=0;i<=10;i++){
+        
+        sum += i;
+
+        printf("the sum of the numbers: %d\n",sum);
+    }
+}
